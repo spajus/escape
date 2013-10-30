5eaf62216043f684482961768714e742ce829a15
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2013-10-30 08:45:41 +0200

    Establish heroku DB connection

--- server/server.rb
+++ server/server.rb
@@ -5,12 +5,13 @@ require_relative 'escape/escape'
 
 require 'pry-nav' if settings.development?
 
+
 root_dir = File.join(File.dirname(File.dirname(__FILE__)))
 client_dir = File.join(root_dir, 'client')
 set :public_folder, client_dir
-set :database_file, File.join(root_dir, 'db', 'database.yml')
 enable :sessions
 set :session_secret, Escape::Constants::SESSION_SECRET
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
 
 get '/' do
   send_file File.join(client_dir, 'index.html')
